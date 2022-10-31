4b656e63b987fff5c8c09d2a95a2511b34a31c5c
Jaris <90911153+JohnARI@users.noreply.github.com>
2022-10-31 11:47:57 +0100

    initial commit

---
+++ src/test/java/myCat/dashboard/DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package myCat.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
