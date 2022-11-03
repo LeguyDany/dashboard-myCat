4ee336f043c0db5f503a19d1609af723c2bb8538
Arnaud Moreau <arnaud.moreau@epitech.eu>
2022-11-03 16:07:21 +0100

    first commith

--- back/src/main/java/mycat/back/controller/Spotify.java
+++ back/src/main/java/mycat/back/controller/Spotify.java
@@ -1,24 +1,21 @@
 package mycat.back.controller;
 
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-// response_type=code&client_id=CLIENT_ID&scope=user-read-private%20user-read-email&redirect_uri=http%3A%2F%2Flocalhost%3A8888%2Fcallback&state=futlmxmpohocbxib
+
 @RestController
-@RequestMapping("/api/spotify/")
+@RequestMapping("/api/spotify/login")
 public class Spotify {
 
-    @GetMapping("login")
+    @RequestMapping(method = RequestMethod.GET)
     public ModelAndView redirectWithUsingRedirectPrefix(ModelMap model){
         model.addAttribute("response_type", "code");
         model.addAttribute("client_id","80db1bd3fc9845ad9a188627e68e774a");
         model.addAttribute("scope","user-read-private user-read-email");
         model.addAttribute("redirect_uri", "http://localhost:8080/sayhello");
         model.addAttribute("state", "azertyuiopqsdfgh");
-        return new ModelAndView("redirect:/https://accounts.spotify.com/authorize?", model);
+        return new ModelAndView("redirect:https://accounts.spotify.com/authorize?", model);
     }
 
 }
