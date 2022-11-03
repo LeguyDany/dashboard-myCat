4a5ff0f06415f12b3e16f20615201c70f9cd6cdc
Arnaud Moreau <arnaud.moreau@epitech.eu>
2022-11-03 15:15:53 +0100

    first commith

--- back/src/main/java/mycat/back/controller/Spotify.java
+++ back/src/main/java/mycat/back/controller/Spotify.java
@@ -5,14 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import mycat.back.models.SpotifyInfo;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/api/spotify/")
 public class Spotify {
-    Spotify data = new Spotify();
-    String test = data.
+
 
     @GetMapping("login")
     public ModelAndView redirectWithUsingRedirectPrefix(ModelMap model){
