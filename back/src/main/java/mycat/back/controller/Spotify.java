0476a208d91171b8d512f7b5801d8e65ff59d67e
Arnaud Moreau <arnaud.moreau@epitech.eu>
2022-11-03 15:47:12 +0100

    first commith

--- back/src/main/java/mycat/back/controller/Spotify.java
+++ back/src/main/java/mycat/back/controller/Spotify.java
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
+// response_type=code&client_id=CLIENT_ID&scope=user-read-private%20user-read-email&redirect_uri=http%3A%2F%2Flocalhost%3A8888%2Fcallback&state=futlmxmpohocbxib
 @RestController
 @RequestMapping("/api/spotify/")
 public class Spotify {
 
-
     @GetMapping("login")
     public ModelAndView redirectWithUsingRedirectPrefix(ModelMap model){
-        model.addAttribute("attribute", "redirectWithRedirectPrefix");
+        model.addAttribute("response_type", "code");
+        model.addAttribute("client_id","80db1bd3fc9845ad9a188627e68e774a");
+        model.addAttribute("scope","user-read-private user-read-email");
+        model.addAttribute("redirect_uri", "http://localhost:8080/sayhello");
+        model.addAttribute("state", "azertyuiopqsdfgh");
         return new ModelAndView("redirect:/https://accounts.spotify.com/authorize?", model);
     }
 
