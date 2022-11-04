025d431eec999a2c26f25c300a9fabbf4aeecbb0
Jaris <90911153+JohnARI@users.noreply.github.com>
2022-11-04 09:16:04 +0100

    OAuth 2 avec GitHub
    Création du OAuth 2 via GitHub intégré

--- back/src/main/java/mycat/back/BackApplication.java
+++ back/src/main/java/mycat/back/BackApplication.java
@@ -1,8 +1,4 @@
 package mycat.back;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
