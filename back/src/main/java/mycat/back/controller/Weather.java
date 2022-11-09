b2483ea8726855cc78ef662123c097c6373376f2
Leguy Dany <dany.leguy@hetic.net>
2022-11-09 16:17:58 +0100

    Merge John 1

---
+++ back/src/main/java/mycat/back/controller/Weather.java
@@ -0,0 +1,42 @@
+package mycat.back.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.twitter.clientlib.ApiException;
+import com.twitter.clientlib.TwitterCredentialsBearer;
+import com.twitter.clientlib.TwitterCredentialsOAuth2;
+import com.twitter.clientlib.api.TwitterApi;
+import com.twitter.clientlib.model.CreateTweetRequest;
+import com.twitter.clientlib.model.CreateTweetRequestPoll;
+import com.twitter.clientlib.model.SingleTweetLookupResponse;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/weather/")
+public class Weather {
+
+    @GetMapping('/post')
+    @PostMapping()
+    public {
+
+    }
+
+
+
+}
