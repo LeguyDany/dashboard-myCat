6a87433f853434100d9d94c83980c88b58162669
Leguy Dany <dany.leguy@hetic.net>
2022-11-10 14:20:35 +0100

    Front weather widget finished

--- back/src/main/java/mycat/back/controller/Weather.java
+++ back/src/main/java/mycat/back/controller/Weather.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -21,15 +23,15 @@ import java.util.*;
 public class Weather {
 
     @PostMapping("/post")
-    public String getWeather(HttpServletRequest request, HttpServletResponse response, @RequestBody LinkedHashMap obj) throws IOException, ServletException, InterruptedException, URISyntaxException {
+    public Map getWeather(HttpServletRequest request, HttpServletResponse response, @RequestBody LinkedHashMap obj) throws IOException, ServletException, InterruptedException, URISyntaxException {
 
-        System.out.println( obj.get("address"));
+        String address = URLEncoder.encode(obj.get("address").toString(), "UTF-8");
 
-        String geoUrl = "https://dev.virtualearth.net/REST/v1/Locations?addressLine=" + obj.get("address") + "&key=Ar-52ZP0daURxbJY6zt-9pkdluk67GmaCl6C_l6vq0UH91dnHGr5y1l8YsI2r1cI";
+        URL geoUrl = new URL("https://dev.virtualearth.net/REST/v1/Locations?addressLine=" + address + "&key=Ar-52ZP0daURxbJY6zt-9pkdluk67GmaCl6C_l6vq0UH91dnHGr5y1l8YsI2r1cI");
 
         HttpRequest req = HttpRequest.newBuilder()
                 .GET()
-                .uri(URI.create(geoUrl))
+                .uri(URI.create(geoUrl.toString()))
                 .header("Content-Type", "application/json")
                 .build();
 
@@ -39,15 +41,26 @@ public class Weather {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readValue(res.body(), JsonNode.class);
 
-        System.out.println(jsonNode.get("resourceSets").get("resources").get(0).get("point").get("coordinate").toString());
+        JsonNode coordinates = jsonNode.get("resourceSets").get(0).get("resources").get(0).get("point").get("coordinates");
 
-        /*ArrayList<JsonNode> coordinate = new ArrayList<>();
-        coordinate.add(jsonNode.get("resourceSets").get("resources").get(0).get("point").get("coordinate"));*/
+        URL weatherUrl = new URL("https://api.open-meteo.com/v1/forecast?latitude=" + coordinates.get(0) + "&longitude=" + coordinates.get(1) + "&hourly=temperature_2m,precipitation,cloudcover");
 
-        String weatherUrl = "https://api.open-meteo.com/v1/forecast?latitude=";
+        HttpRequest req2 = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(weatherUrl.toString()))
+                .header("Content-Type", "application/json")
+                .build();
 
-        // https://api.open-meteo.com/v1/forecast?latitude=49.0496&longitude=2.0416&hourly=temperature_2m,precipitation,cloudcover
+        HttpResponse<String> res2 = httpClient.send(req2, HttpResponse.BodyHandlers.ofString());
+        JsonNode jsonNode2 = objectMapper.readValue(res2.body(), JsonNode.class);
 
-        return "Bonjour";
+        Map<String, JsonNode> weather = new HashMap<>();
+        weather.put("hour",jsonNode2.get("hourly").get("time"));
+        weather.put("temp",jsonNode2.get("hourly").get("temperature_2m"));
+        weather.put("rain",jsonNode2.get("hourly").get("precipitation"));
+        weather.put("cloud",jsonNode2.get("hourly").get("cloudcover"));
+        weather.put("address", jsonNode.get("resourceSets").get(0).get("resources").get(0).get("name"));
+
+        return weather;
     }
 }
