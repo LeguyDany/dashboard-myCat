28743b287c2d110dd6f2d34b71daea1b198cd159
Leguy Dany <dany.leguy@hetic.net>
2022-11-12 21:23:49 +0100

    Front widget Twitter Post 70% over

--- front/src/widgets/twitter.tsx
+++ front/src/widgets/twitter.tsx
@@ -13,10 +13,13 @@ import pollIcon from '../assets/icons/chat-poll-line.svg';
 
 // ============================================= Components =============================================
 // ------------------------------------- Interfaces -------------------------------------
-
+interface pollLength{
+    days:string;
+    hours:string;
+    minutes:string;
+}
 
 // ------------------------------------- Child -------------------------------------
-
 
 
 // ------------------------------------- Main component -------------------------------------
@@ -30,6 +33,11 @@ export function PostTweet(){
     const [reply_settings, setReplySettings] = useState<String>();
     const [imgSelect, setImgSelect] = useState<string>("");
     const [pollSelect, setPollSelect] = useState<string>("");
+    const [choice1, setChoice1] = useState<string>("");
+    const [choice2, setChoice2] = useState<string>("");
+    const [choice3, setChoice3] = useState<string>("");
+    const [choice4, setChoice4] = useState<string>("");
+    const [pollLength, setPollLength] = useState<pollLength>({days:"0", hours:"0", minutes:"0"});
 
 
     const handlePost = async () => {
@@ -77,10 +85,38 @@ export function PostTweet(){
 
             <hr className="lightSeparator"/>
 
-            {imgSelect}
-            <div className={"tweetPostImgUpload" + imgSelect=="twitterPostOptionSelected"?"bojnour":""}>
+            {/* --------------------------------- media upload ---------------------- */}
+            <form className={`tweetPostImgUpload ${imgSelect=="twitterPostOptionSelected"?"":"displayNone"}`}>
                 {media}
                 <input type="file" name="resume" id="resume" onChange={e => uploadFile(e)} />
+                <hr className="lightSeparator"/>
+            </form>
+
+            {/* --------------------------------- poll ---------------------- */}
+            <div className={`tweetPostPoll ${pollSelect=="twitterPostOptionSelected"?"":"displayNone"}`}>
+                <form>
+                    <legend>Poll</legend>
+                    <input type="text" placeholder="Choice 1" onChange={e => setChoice1(e.target.value)}/>
+                    <input type="text" placeholder="Choice 2" onChange={e => setChoice2(e.target.value)}/>
+                    <input type="text" placeholder="Choice 3 (optional)" onChange={e => setChoice3(e.target.value)}/>
+                    <input type="text" placeholder="Choice 4 (optional)" onChange={e => setChoice4(e.target.value)}/>
+                    <div>
+                        <label>
+                            Days <br/>
+                            <input type="number" onChange={e => {pollLength["days"]=e.target.value}} name="days" id="days" min={0} max={7}/>
+                        </label>
+                        <label>
+                            Hours <br/>
+                            <input type="number" onChange={e => {pollLength["hours"]=e.target.value}} name="hours" id="hours" min={0} max={23}/>
+                        </label>
+                        <label>
+                            Minutes <br/>
+                            <input type="number" onChange={e => {pollLength["minutes"]=e.target.value}} name="minutes" id="minutes" min={0} max={59}/>
+                        </label>
+                    </div>
+
+
+                </form>
                 <hr className="lightSeparator"/>
             </div>
 
